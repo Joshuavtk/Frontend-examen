c74f37239f6c35a0ad4edcdb05d2dfba62fd2687
Joshua <joshuavtk@hotmail.com>
2018-07-05 16:23:38 +0200

    Add complete Impression / Indruk tab
    Page almost completely generated based on the data

--- src/components/Impression.vue
+++ src/components/Impression.vue
@@ -1,62 +1,36 @@
 <template>
-    <div data-step="2" class="step-pane">
-        <form action="#" data-parsley-namespace="data-parsley-" data-parsley-validate=""
-        novalidate="" class="group-border-dashed">
-            <div class="form-group row">
-                <div class="col-sm-7">
-                    <h3 class="wizard-title">Notifications</h3>
-                </div>
-            </div>
-            <div class="form-group row align-items-center">
-                <div class="col-sm-7">
-                    <label class="col-form-label">E-Mail Notifications</label>
-                    <p>This option allow you to recieve email notifications by us.</p>
-                </div>
-                <div class="col-sm-3">
-                    <div class="switch-button">
-                        <input type="checkbox" checked="" name="swt1" id="swt1">
-                        <span>
-                            <label for="swt1"></label>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group row align-items-center">
-                <div class="col-sm-7">
-                    <label class="col-form-label">Phone Notifications</label>
-                    <p>Allow us to send phone notifications to your cell phone.</p>
-                </div>
-                <div class="col-sm-3">
-                    <div class="switch-button">
-                        <input type="checkbox" checked="" name="swt2" id="swt2">
-                        <span>
-                            <label for="swt2"></label>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group row align-items-center">
-                <div class="col-sm-7">
-                    <label class="col-form-label">Global Notifications</label>
-                    <p>Allow us to send notifications to your dashboard.</p>
-                </div>
-                <div class="col-sm-3">
-                    <div class="switch-button">
-                        <input type="checkbox" checked="" name="swt3" id="swt3">
-                        <span>
-                            <label for="swt3"></label>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-12">
-                    <button data-wizard="#wizard1"
-                    class="btn btn-secondary btn-space wizard-previous">Previous</button>
-                    <button data-wizard="#wizard1"
-                    class="btn btn-primary btn-space wizard-next">Next Step</button>
-                </div>
-            </div>
-        </form>
+    <div>
+        <div v-for="(sub_categories, index) in categories" :key="index">
+            <h4>{{index}}</h4>
+            <InlineRadio v-for="(category, index) in sub_categories" :key="index" :title="category" :array="results"/>
+        </div>
     </div>
 </template>
+
+<script>
+import InlineRadio from './InlineRadio';
+
+export default {
+    components: {
+        InlineRadio,
+    },
+
+    data() {
+        return {
+            categories: {
+                'Werkhouding': [
+                    'Concentratie',
+                    'Werktempo',
+                    'Zelfstandig werken in klas',
+                ],
+                'Instelling': ['Motivatie', 'Doorzettingsvermogen'],
+                'Vaardigheden': [
+                    'Communicatieve vaardigheden',
+                    'Sociale vaardigheden',
+                ],
+            },
+            results: ['Goed', 'Voldoende', 'Zwak', 'Onvoldoende'],
+        };
+    },
+};
+</script>
